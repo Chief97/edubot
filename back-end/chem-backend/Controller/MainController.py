35d6665a239440cd87fcf59b87020e881e419628
Vinu <sahanigamage@gmail.com>
2020-08-05 14:45:45 +0530

    Updated WH question generation.(controller,service,PreProcess and QuestionFormation was changed)

--- back-end/chem-backend/Controller/MainController.py
+++ back-end/chem-backend/Controller/MainController.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from Service.Self_Assess_Question_Generation.QuestionGenerationService import QuestionGenerationService
 
@@ -7,11 +7,18 @@ app = Flask(__name__)
 questionGenerationService = QuestionGenerationService()
 
 
-@app.route('/iSentences', methods=['POST'])
+@app.route('/selfEvaluate/questionGeneration/importantSentences', methods=['POST'])
 def allImportantSentences():
     textInput = request.get_json()['text']
     sentences = questionGenerationService.importantSentences(textInput)
     return sentences
+
+
+@app.route('/selfEvaluate/questionGeneration/allQuestions', methods=['POST'])
+def allQuestions():
+    textInput = request.get_json()['text']
+    allQuestionsForPara = questionGenerationService.executeQuestionGeneration(textInput)
+    return jsonify(allQuestionsForPara)
 
 
 app.run(debug=True)
