a5d8fc5f2345e4818c7338a2f69ab5cd1c97fed4
Satyagit <satyagit@emojot.com>
2020-08-11 22:32:20 +0530

    Sathya

--- front-end/chem-frontend/src/app/app.module.ts
+++ front-end/chem-frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChaptersChemComponent } from './chapters-chem/chapters-chem.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChaptersChemComponent
   ],
   imports: [
     BrowserModule,
