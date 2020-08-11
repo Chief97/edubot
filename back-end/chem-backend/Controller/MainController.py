239bab0037bbddd63d22d248a8f1bef6859377da
Satyagit <satyagit@emojot.com>
2020-08-11 22:30:27 +0530

    Sathys

--- back-end/chem-backend/Controller/MainController.py
+++ back-end/chem-backend/Controller/MainController.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 
 from Service.Self_Assess_Question_Generation.QuestionGenerationService import QuestionGenerationService
+from Service.Self_Learn_Doubt_Response.DoubtResponseService import GenerateResponse
 
 app = Flask(__name__)
 
 questionGenerationService = QuestionGenerationService()
+generateResponse = GenerateResponse()
 
 
 @app.route('/iSentences', methods=['POST'])
@@ -12,6 +14,15 @@ def allImportantSentences():
     textInput = request.get_json()['text']
     sentences = questionGenerationService.importantSentences(textInput)
     return sentences
+
+@app.route('/scraping' , methods=[''])
+def webScraper():
+    textInput = request.get_json()['text']
+    webScrape = generateResponse.webScraper(textInput)
+
+@app.route('/searchKeywords', methods=['GET'])
+def searchKeywords ():
+
 
 
 app.run(debug=True)
--- back-end/chem-backend/Service/Self_Learn_Doubt_Response/DoubtResponseService.py
+++ back-end/chem-backend/Service/Self_Learn_Doubt_Response/DoubtResponseService.py
@@ -1,1 +1,9 @@
-#doubt response service
+from General.Self_Learn_Doubt_Response.webScraper import ChemistrySpider
+
+class GenerateResponse():
+
+    def webScraper(self, textInput):
+        spyder = ChemistrySpider
+        webScrape = spyder.parse(textInput)
+
+
