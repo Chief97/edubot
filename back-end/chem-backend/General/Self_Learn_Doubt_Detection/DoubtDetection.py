38be8ba121dbc5fc9c371f8aafa27f678562e5c9
karthi <it17386746@my.sliit.lk>
2020-09-20 12:34:35 +0530

    Commit Doubt Detection

--- back-end/chem-backend/General/Self_Learn_Doubt_Detection/DoubtDetection.py
+++ back-end/chem-backend/General/Self_Learn_Doubt_Detection/DoubtDetection.py
@@ -1,3 +1,10 @@
+"""
+    NLP techniques for pre-processing,
+    encode the pre-processed words by creating training data,
+    Build Random Forest Classifier model,
+    train the model
+"""
+
 import random
 import nltk
 import json
@@ -5,89 +12,66 @@ import numpy as np
 import pickle
 from nltk.stem.lancaster import LancasterStemmer
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split  # Using Skicit-learn to split data into training and testing sets
+from sklearn.model_selection import train_test_split
 
 nltk.download('punkt')
 stemmer = LancasterStemmer()
 
 
 class DoubtDetection(object):
-    # opening JSON file
-    file = open('../intents.json',)
+    # import dataset as JSON file
+    file = open('../intents.json', )
     intents = json.load(file)
 
-    # Tokenizing
-    token_words = []  # each tokenized words
-    pattern_id = []  # class name
-    corpus = []  # stemmed words as class wise
-    # ignore = ['?']
+    # Tokenizing the sentences in the dataset
+    token_words = []  # assigning array for tokenized words
+    intent_ids = []   # assigning array for intent IDs
+    corpus = []       # assigning array for tokenized words, respective intent ID
 
     for intent in intents['intents']:
         for pattern in intent['patterns']:
             words = nltk.word_tokenize(pattern)
             token_words.extend(words)
-            corpus.append((words, intent['class']))
+            corpus.append((words, intent['intent_id']))
 
-            if intent['class'] not in pattern_id:
-                pattern_id.append(intent['class'])  # add class to pattern_id
-    # print(pattern_id)
-    # print(token_words)
-    # print(corpus)
+            if intent['intent_id'] not in intent_ids:
+                intent_ids.append(intent['intent_id'])
 
-    # Stemming
-    stemmed_words = [stemmer.stem(word.lower()) for word in token_words if
-                     word not in ["?", "'s", "+", ",", "(", ")", "-"]]
-    stemmed_words = sorted(list(set(stemmed_words)))  # set used remove duplicate stemmed words
+    # Stemming the tokenized words
+    stemmed_words = [stemmer.stem(word.lower()) for word in token_words
+                     if word not in ["?", "'s", "+", ",", "(", ")", "-"]]
+    stemmed_words = sorted(list(set(stemmed_words)))  # remove duplicate stemmed words
+    intent_ids = sorted(list(set(intent_ids)))  # remove duplicate intent_id words
 
-    pattern_id = sorted(list(set(pattern_id)))  # remove deplicate class words
-
-    # print(len(corpus), " Tokenized sentence class wise : ", corpus)
-    # print(len(pattern_id) ," Classes : ", pattern_id)
-    # print(" unique stemmed words : ", stemmed_words)
-    # print(len(stemmed_words))
-
-    # Create training data
+    # Create training data in encoded form
     training = []
-    output = []
-
-    # Create an empty array for output
-    output_empty = [0] * len(pattern_id)
+    output_empty = [0] * len(intent_ids)
 
     # Create training set, bag of words for each sentence
     for doc in corpus:
-        # initialize bag of words
         bag = []
-        # List of tokenized words for the pattern
-        pattern_words = doc[0]
-        # Stemming each word
-        pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+        intent_words = doc[0]
+        intent_words = [stemmer.stem(word.lower()) for word in intent_words]
 
         # Create bag of words array
         for w in stemmed_words:
-            bag.append(1) if w in pattern_words else bag.append(0)
-        # print(bag)
-        output_feature = list(output_empty)
-        output_feature[pattern_id.index(doc[1])] = 1
+            bag.append(1) if w in intent_words else bag.append(0)
 
+        output_feature = list(output_empty)
+        output_feature[intent_ids.index(doc[1])] = 1
         training.append([bag, output_feature])
-    # print(training)
-    # s Shuffling features and training it into np.array
-    random.shuffle(training)
+
+    random.shuffle(training)  # Shuffling features
     training = np.array(training)
 
     # Creating training lists
     train_x = list(training[:, 0])
     train_y = list(training[:, 1])
 
-    # print(train_x)
-    # print(train_y)
-
     # Split the data into training and testing sets
-    train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size=0.25)
+    train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size=0.02)
 
-    # Import the model we are using
-
-    # Instantiate model with 1000 decision trees
+    # Instantiate model with 10 decision trees
     rf = RandomForestRegressor(n_estimators=10, random_state=1)
 
     # Train the model on training data
@@ -95,60 +79,15 @@ class DoubtDetection(object):
 
     # Test the models accuracy on the training data
     model = rf
-    # print("Accuracy : ", model.score(train_x, train_y))
+    print("Train Accuracy : ", model.score(train_x, train_y))
+    print("Test Accuracy : ", model.score(test_x, test_y))
 
+    # save the model using pickle
     pickle.dump({
         "stemmed_words": stemmed_words,
-        "pattern_id": pattern_id,
+        "intent_ids": intent_ids,
         "train_x": train_x,
         "train_y": train_y},
         open('training_data', 'wb')
     )
 
-    # restoring all the data structures
-    data = pickle.load(open("training_data", "rb"))
-    stemmed_words = data['stemmed_words']
-    pattern_id = data['pattern_id']
-    train_x = data['train_x']
-    train_y = data['train_y']
-
-    with open('../intents.json') as json_data:
-        intents = json.load(json_data)
-
-    def clean_up_sentence(self, sentence):
-        # tokenizing the pattern
-        sentence_words = nltk.word_tokenize(sentence)
-        # stemming each word
-        sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
-        return sentence_words
-
-    # returning bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-    def bow(self, sentence, words, show_details=False):
-        # tokenizing the pattern
-        sentence_words = self.clean_up_sentence(sentence)
-        # generating bag of words
-        bag = [0] * len(words)
-        for s in sentence_words:
-            for i, w in enumerate(words):
-                if w == s:
-                    bag[i] = 1
-                    if show_details:
-                        print("found in bag: %s" % w)
-
-        return (np.array(bag))
-
-    def classify(self, sentence):
-        THRESHOLD = 0.3
-
-        # generate probabilities from the model
-        results = self.model.predict([self.bow(sentence, self.stemmed_words)])[0]
-        # filter out predictions below a threshold
-        results = [[i, r] for i, r in enumerate(results) if r > THRESHOLD]
-        # sort by strength of probability
-        results.sort(key=lambda x: x[1], reverse=True)
-        return_list = []
-        for r in results:
-            return_list.append((self.pattern_id[r[0]], r[1]))
-        # return tuple of intent(i.e. tag) and probability
-
-        return return_list
