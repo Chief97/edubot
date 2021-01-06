704c659315f9d23cead35c989031a7240a1b48fd
atheeq <it17137492@my.sliit.lk>
2021-01-06 18:50:23 +0530

    Final Merge Commit

--- front-end/chem-frontend/src/app/main-module/main-component/main-module.module.ts
+++ front-end/chem-frontend/src/app/main-module/main-component/main-module.module.ts
@@ -15,13 +15,13 @@ import {HttpClientModule} from "@angular/common/http";
 import {GeneralServiceService} from "../../services/general-service.service";
 import {SummaryPopupComponent} from "../../components/summary-popup/summary-popup.component";
 import {ChartsModule} from "ng2-charts";
-import {DataDisplayComponent} from '../../components/data-display/data-display.component';
+
 
 
 
 
 @NgModule({
-  declarations: [DataDisplayComponent, MainComponentComponent,HeaderComponentComponent,HomeComponentComponent,ProfileComponentComponent,AboutComponentComponent,LearnComponentComponent,AssessComponentComponent,MainContentComponentComponent,SummaryPopupComponent],
+  declarations: [ MainComponentComponent,HeaderComponentComponent,HomeComponentComponent,ProfileComponentComponent,AboutComponentComponent,LearnComponentComponent,AssessComponentComponent,MainContentComponentComponent,SummaryPopupComponent],
   exports: [SummaryPopupComponent],
   imports: [
     CommonModule,
